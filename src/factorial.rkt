02fe550540281b053d43e0646d3ce74b30763155
waltervargas <walter@flux7.com>
2017-01-27 23:50:40 -0400

    Adds factorial and car exercise

---
+++ src/factorial.rkt
@@ -0,0 +1,21 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname factorial) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+; Factorial in Scheme
+
+; A linear iterative factorial
+(define (factorial n) 
+  (fact-iter 1 1 n))
+
+(define (fact-iter product counter max-count)
+  (if (> counter max-count)
+      product
+      (fact-iter (* counter product)
+                 (+ counter 1)
+                 max-count)))
+
+; A linear recursive factorial
+(define (factorial2 n)
+  (if (= n 1) 
+      1 
+      (* n (factorial (- n 1)))))
